a805bdc57b88b9c6df4545cc13b49ae43cb3a7c2
Jose Campos <camposplaza77@gmail.com>
2022-12-03 18:19:22 +0100

    base folder and files created

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
-const { src, dest } = require("gulp");
+const { src, dest, watch } = require("gulp");
 const sass = require("gulp-sass")(require('sass'));
 
 function css(done){
-    src('src/scss/app.scss').pipe(sass()).pipe(dest('build/css'));
+    src('src/scss/**/*.scss').pipe(sass()).pipe(dest('build/css'));
+
+    done();
+}
+
+function dev(done){
+    watch('src/scss/**/*.scss', css);
 
     done();
 }
 
 exports.css = css;
+exports.dev = dev;
